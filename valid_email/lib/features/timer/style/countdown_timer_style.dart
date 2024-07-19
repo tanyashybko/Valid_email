3151948134386a58ed30318464232dd2bd9cb16f
HrushaTanusha <t.shybko04@gmail.com>
2024-07-19 11:57:34 +0300

    Added stopwatch and timer

---
+++ valid_email/lib/features/timer/style/countdown_timer_style.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+TextStyle countdownTimerTextStyle() {
+  return const TextStyle(
+    fontSize: 48.0,
+    fontWeight: FontWeight.bold,
+    color: Colors.black,
+  );
+}
---
+++ valid_email/lib/widgets/countdown_timer_widget.dart
@@ -0,0 +1,57 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:valid_email/l10n/l10n.dart';
+import '../features/timer/logic/countdown_timer_logic.dart';
+import '../features/timer/style/countdown_timer_style.dart';
+import '../localization/language_switcher.dart';
+
+class CountdownTimer extends StatefulWidget {
+  final Duration duration;
+
+  const CountdownTimer({super.key, required this.duration});
+
+  @override
+  State<CountdownTimer> createState() => _CountdownTimerState();
+}
+
+class _CountdownTimerState extends State<CountdownTimer> {
+  late Duration remainingTime;
+  Timer? timer;
+
+  @override
+  void initState() {
+    super.initState();
+    remainingTime = widget.duration;
+    timer = startTimer(remainingTime, (newTime) {
+      setState(() {
+        remainingTime = newTime;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    timer?.cancel();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final l10n = context.l10n;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(l10n.timerTitle),
+        actions: const [
+          LanguageSwitcher(),
+        ],
+      ),
+      body: Center(
+        child: Text(
+          formatDuration(remainingTime),
+          style: countdownTimerTextStyle(),
+        ),
+      ),
+    );
+  }
+}
